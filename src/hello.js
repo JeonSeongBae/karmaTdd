eec5b9da2b7c02c79d75df0feacff05f48cba27a
JeonSeongBae <tjdqo6042@naver.com>
2017-11-06 17:16:12 +0900

    new hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,15 @@
 var Hello = {
-    message: 'Hello',
-    greeting() {
-        return `this.message ${this.getName()}`;
-    },
+    // message: 'Hello',
+    // greeting() {
+    //     return `this.message ${this.getName()}`;
+    // },
+    // getName() {
+    //     return 'World';
+    // }
     getName() {
+        const req = new XMLHttpRequest();
+        req.open('GET', 'http://name', true);
+        req.send(null);
         return 'World';
     }
 };
